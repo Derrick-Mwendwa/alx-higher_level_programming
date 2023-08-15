542b834b093721e5548e1e2f66a66ca7ef6f8c6d
Derrick Mwendwa <derekmwendwa14@gmail.com>
2023-08-16 00:55:44 +0300

    update task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
         return (None)
 
     big_int = my_list[0]
-    
+
     for i in range(len(my_list)):
         if my_list[i] > big_int:
             big_int = my_list[i]
