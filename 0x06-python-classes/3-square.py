1b60d8b9873ded2a787200391e02141659ebc95f
Derrick Mwendwa <derekmwendwa14@gmail.com>
2024-04-05 12:03:44 +0300

    Add docstrings to Square class methods

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""Define a Square."""
+
 
 class Square:
+    """This is a class representing a square."""
+
     def __init__(self, size=0):
+        """
+        Initialize a Square.
+
+        Args:
+            size (int): The size of the square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -9,4 +19,5 @@ class Square:
         self.__size = size
 
     def area(self):
+        """Return the area of the square."""
         return (self.__size * self.__size)
