805523c0d6c74e012f90c26defb96ad8c8e4e552
Derrick Mwendwa <derekmwendwa14@gmail.com>
2024-04-05 11:58:37 +0300

    Add docstrings to Square class constructor

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""Define a Square."""
+
 
 class Square:
+    """This is a class representing a square."""
+
     def __init__(self, size):
+        """
+        Initialize a Square.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
